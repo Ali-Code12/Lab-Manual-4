1025784feb3da2e687af41fc8f1751bf1949e9f1
Ali-Code12 <147544910+Ali-Code12@users.noreply.github.com>
2023-10-19 16:42:43 +0500

    Add files via upload

---
+++ lab task4.0.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+//Task 1
+int main()
+{
+	int i;
+	int sum;
+	cin>>sum;
+	for(i=1;i<11;i++)
+	{
+		sum+=i;
+		
+	}
+	cout<<"Sum of first 10 numbers is: " <<sum<<endl;
+   return 0;
+}
+/*//Task 2
+int main()
+{
+	int num;
+	cout<<"Enter the number you want table for: "<<endl;
+	cin>>num;()
+
+	int i;
+	for(i=1;i<11;i++)
+	{
+		cout<<num<<" x "<<i<<" = "<<num*i<<endl;
+	}
+}
+
+//Task 3
+int main()
+{
+	int num;
+	cout<<"Enter the number to take factorial of: "<<endl;
+	cin>>num;
+	int i;
+	int fac=num;
+	for(i=(num-1);i>0;i--)
+	{
+		fac*=i;
+	}
+	cout<<"Factorial is "<<fac<<endl;
+	return 0;
+}
+
+//Task 4
+int main()
+{
+	int a;
+	int b;
+	int d;
+	cout<<"Enter First two number of sequence : "
+	cin>>a>>b;
+	int ran;
+	cout<<" Enter range "<<endl;
+	cin>>ran;
+	for(d=0;d<range;d++)
+	{
+		if(d==0)
+		{
+			cout<<a;
+			continue;
+		}
+		else if(d==1)
+		{
+			cout<<b;
+			continue;
+		}
+		nterm = a +b;
+		a=b;
+		b=nterm;
+		cout<<nterm;
+	}
+	
+}*/
+
+
